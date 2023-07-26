8e84136468e56ac3319220dbb2007360e557e406
Alec Christiansen <achristiansen@cumberlandcountync.gov>
2023-07-26 16:29:50 -0400

    update script.js

--- script.js
+++ script.js
@@ -10,10 +10,44 @@ returnMovies(APILINK);
 
 function returnMovies(url){
     fetch(url).then(res => res.json())
-    .then(function(data)){
+    .then(function(data){
         console.log(data.results);
-        data.results.forEach(element => {
-            const div = document.createElement('div');
+        data.results.array.forEach(element => {
+            const div_card = document.createElement('div');
+            div_card.setAttribute('class', 'card');
+            const div_row = document.createElement('div');
+            div_card.setAttribute('class', 'row');
+            const div_column = document.createElement('div');
+            div_card.setAttribute('class', 'column');
+            const image= document.createElement('img');
+            div_card.setAttribute('class', 'thumbnail');
+            div_card.setAttribute('id', 'image');
+            const title = document.createElement('h3');
+            div_card.setAttribute('id', 'title');
+            const center = document.createElement('center');
+
+            title.innerHTML = `${element.title}`;
+            image.src = IMG_PATH + element.poster_path;
+
+            center.appendChild(image);
+            div_card.appendChild(center);
+            div_card.appendChild(title);
+            div_column.appendChild(div_card);
+            div_row.appendChild(div_row);
+
+            main.appendChild(div_row);
         });
-    }
+    };
 }
+
+FormData.addEventListner("submit", (e) => {
+    e.preventDefault();
+    main.innerHTML = ''
+
+    const searchItem = search.value;
+
+    if (searchItem) {
+        returnMovies(SEARCHAPI + searchItem);
+        search.value = '';
+    }
+});
