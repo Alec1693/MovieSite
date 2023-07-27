e3341fd5c59cd1e4ae0a589fe05b464b397c6678
Alec1693 <alecchristiansen@gmail.com>
2023-07-27 16:36:08 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=a9f89fece1309f8a1532b5cdd5fc5f07';
+const APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=a9f89fece1309f8a1532b5cdd5fc5f07&page=1';
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=a9f89fece1309f8a1532b5cdd5fc5f07&query=";
 
